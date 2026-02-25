62011fc65fae2bd974bfa17582420769473e68e5
Dany <dany@gmail.com>
2026-02-24 22:30:57 -0600

    Add Styles Nav Dashboard

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,10 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import LoginView from "./views/LoginView";
 import RegisterView from "./views/RegisterView";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
 
 const Router = () => {
 
@@ -12,6 +15,11 @@ const Router = () => {
                     <Route path="/auth/login" element={<LoginView /> }/>
                     <Route path="/auth/register" element={<RegisterView /> }/>
                 </Route>
+
+                <Route path="/admin" element={<AppLayout />}>
+                    <Route index={true}  element={<LinkTreeView />}/>
+                    <Route path="profile"  element={<ProfileView />}/>
+                </Route>    
             </Routes>
         </BrowserRouter>
     )
---
+++ src/views/LinkTreeView.tsx
@@ -0,0 +1,10 @@
+
+const LinkTreeView = () => {
+  return (
+    <div>   
+        LinkTreeView
+    </div>
+  )
+}
+
+export default LinkTreeView
